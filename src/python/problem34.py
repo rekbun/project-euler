296d630254543b5c98ad9c0ed75f468bea1ed211
rakesh <rkchoudhary0208@gmail.com>
2014-04-02 01:09:35 +0530

    problem 34

---
+++ src/python/problem34.py
@@ -0,0 +1,20 @@
+
+
+
+def fact(n):
+	if(n==0):
+		return n
+	return reduce(lambda x,y:x*y,range(1,n+1))
+
+ret=0
+for i in range(10,2540161):
+	sum=0
+	init=i
+	while(i>0):
+		sum+=fact(i%10)
+		i=i/10
+
+	if(sum==init):
+		ret+=init
+
+print ret		
